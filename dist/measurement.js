cc94ab2726900653c918da7ac8ffa6730b080388
Joel Felsinger <jffelsinger@gmail.com>
2014-12-20 20:32:10 -0500

    Removed logging from browser file

--- dist/measurement.js
+++ dist/measurement.js
@@ -301,18 +301,13 @@ module.exports = ScalarFactory;
 
     'use strict';
 
-    console.log('Build', root);
-
     root.measurement = factory();
 
 }(window, function() {
 
     'use strict';
 
-    var measurement = require('./measurement');
-    console.log(measurement);
-
-    return measurement;
+    return require('./measurement');
 
 }));
 
--- src/browser.js
+++ src/browser.js
@@ -4,17 +4,12 @@
 
     'use strict';
 
-    console.log('Build', root);
-
     root.measurement = factory();
 
 }(window, function() {
 
     'use strict';
 
-    var measurement = require('./measurement');
-    console.log(measurement);
-
-    return measurement;
+    return require('./measurement');
 
 }));
