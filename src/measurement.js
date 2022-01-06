25c1163a1f3ce73edf6e00eb385b92e92a8f8733
Joel Felsinger <jfelsinger@spigot.com>
2022-01-06 17:54:35 -0500

    Remove old js src stuff

--- src/measurement.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var measurement = exports;
-
-measurement.Factory         = require('./factory');
-measurement.Scalar          = require('./scalar');
-measurement.UnitTypes       = require('./unit-types');
-measurement.Unit            = require('./units');
-measurement.CompoundUnit    = require('./compound-units');
